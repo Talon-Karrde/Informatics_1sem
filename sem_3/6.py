0cdcb75df1259c2399892ba7dd88eefdac9d9e5e
Talon-Karrde <145107210+Talon-Karrde@users.noreply.github.com>
2023-09-22 18:10:43 +0300

    Delete sem_3/6.py

--- sem_3/6.py
+++
@@ -1,9 +0,0 @@
-
-#x = [5 4 5 6]
-#y = [6 7 8 9]
-def least_squares(xdata, ydata):
-     a = (sum([x * y for x, y in zip(xdata, ydata)]) - mean(ydata) * sum(xdata)) / (sum([x**2 for x in xdata]) - mean(xdata) * sum(xdata))
-     b = mean(ydata) - a * mean(xdata)
-     return a, b
-
-print(least_squares(x,y))
